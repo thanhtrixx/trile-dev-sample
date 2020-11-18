d17267f0553b6b79001e28c1955719453582f5d2
Tri Le <thanhtrixx@gmail.com>
2020-11-18 22:28:33 +0700

    Add sample for Coroutine Basic Concepts

---
+++ coroutines-basic/src/main/kotlin/dev/trile/coroutinesbasic/SuspendFunction.kt
@@ -0,0 +1,26 @@
+package dev.trile.coroutinesbasic
+
+import kotlinx.coroutines.*
+import kotlin.coroutines.coroutineContext
+
+fun main() = runBlocking(
+  CoroutineName("parent") +
+    newSingleThreadContext("SingleThread") // Dispatcher SingleThread only use one thread
+) {
+
+  launch(CoroutineName("child")) {
+
+    doSomeThingToWait(3000) // release thread in SingleThread for another function
+  }
+
+  doSomeThingToWait(1000) // use thread in SingleThread
+
+}
+
+suspend fun doSomeThingToWait(waitTime: Long) {
+  println("Current context when START: $coroutineContext")
+
+  delay(waitTime) // simulator processing
+
+  println("Current context when END  : $coroutineContext")
+}
